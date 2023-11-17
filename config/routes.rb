7b87bd12c464b65ca54dc23a73be1df0f2101934
Joao Sagorro <joao.sagorro@gmail.com>
2023-11-17 16:08:50 +0000

    Add dotenv--rails

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :bookmarks, only: :destroy
+
   resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create delete]
+    resources :bookmarks, only: %i[new create]
   end
 
   # get '/lists', to: 'lists#index'
